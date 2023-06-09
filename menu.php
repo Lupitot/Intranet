356529028e21c916ce3c8e431e93518065172503
Lupit0t <paul.piauger@ynov.com>
2023-06-09 13:19:33 +0200

    mise en place site

---
+++ menu.php
@@ -0,0 +1,21 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/menu.css">
+    <title>Intranet🍆💦💦🍑😱</title>
+</head>
+<body>
+
+    <div class="content">
+        <div class="contact"></div>
+        <div class="mailbox"></div>
+        <div class="agenda"></div>
+        <div class="note"></div>
+        <div class="folder"></div>
+    </div>
+</body>
+</html>
